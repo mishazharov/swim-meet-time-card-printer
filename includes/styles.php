7a0611d477d7d7c377764e387a0b83a26e8b10ba
Misha Zharov <mishazharov1@gmail.com>
2016-09-05 20:57:35 -0400

    Changes related to errors

--- includes/styles.php
+++ includes/styles.php
@@ -1,3 +1,3 @@
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-<script src="http://fonts.googleapis.com/css?family=Open+Sans" integrity="sha384-d/ZRAAG+/zSYC9CiuG4TH6gbTYypGf86nYYuZIfVuCB9OAdgZua1x6C9B7eDjLlh" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" crossorigin="anonymous" rel="stylesheet"></link>
 <link rel="stylesheet" href="css/main.css" type="text/css">
